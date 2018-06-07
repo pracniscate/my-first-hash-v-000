5fc68b7b623ba36a8f8c665e92cc043e87937a4f
Lorena <lorena.giddens@outlook.com>
2018-06-07 13:16:43 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
-def my_hash = {"Max Richter" => "On The Nature Of Daylight", "Today's Date" => "June 7th"}
+def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-
+  my_hash = {"Max Richter" => "On The Nature Of Daylight", "Today's Date" => "June 7th"}
 end
 
 
