8659857f2d62e68c82774c7663e24702d7f9899d
Lorena <lorena.giddens@outlook.com>
2018-06-07 13:18:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"Max Richter" => "On The Nature Of Daylight", "Animals" => "Giraffe", "Hummingbird"}
+  my_hash = {"Max Richter" => "On The Nature Of Daylight", "Animals" => "Hummingbird"}
 end
 
 
